6aecfc0b21af5ac2b14146edf3397b537a8eab34
VictoriaKwamboka <victoriamonari9815@gmail.com>
2022-03-23 20:30:06 +0300

    bg: Remove text overlay

--- js/script.js
+++ js/script.js
@@ -1,14 +1,1 @@
 
-//make header sticky
-window.onscroll = function() {myFunction()};
-var nav_header = document.getElementsByClassName("nav-header");
-
-var sticky = logo.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    nav_header.classList.add("sticky")
-  } else {
-    nav_header.classList.remove("sticky");
-  }
-}
