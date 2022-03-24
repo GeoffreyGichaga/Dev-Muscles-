43fef6ce838bb62f5c9f8a36a512fdaab60c7e41
VictoriaKwamboka <victoriamonari9815@gmail.com>
2022-03-24 09:26:51 +0300

    Style: Add content to read more

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
+$(document).ready(function () {
+    // let links = ['link_1', 'link_2', 'link_3', 'link_4', 'link_5', 'link_6']
+    // function showContent(){
+    //     for (let i = 0; i < links.length; i++) 
+    //     let link = document.getElementById(links[i]);
 
+    //     if (link.style.display === "block") {
+    //         link.style.display = "none";
+    //     }else{
+    //         link.style.display = "block";
+    //     }
+
+    // }
+
+
+    // let readMoreBtn = document.querySelector('.link_btn');
+    // let text = document.querySelector('.text');
+
+
+    // readMoreBtn.addEventListener('click', (e) => {
+    //     e.preventDefault();
+    //     text.classList.toggle('show-more');
+    //     if (readMoreBtn.innerText === 'Read More') {
+    //         readMoreBtn.innerText = 'Read Less';
+    //     }else{
+    //         readMoreBtn.innerText = 'Read More';
+    //     }
+    
+    
+    // })
+
+    
+});
