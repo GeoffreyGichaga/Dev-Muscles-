dc3d2db99aac37e7a86505c78e8d41d90eb9e4f2
[bonface221] <[mainaboniface221@gmail.com]>
2022-03-24 09:12:38 +0300

    added the videos to the dashboard

---
+++ js/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    $("#course1img").click(function () {
+        $("#course1img").hide();
+        $("#iframe1").show()
+    });
+    $("#iframe1").click(function () {
+        $("#iframe1").hide();
+        $("#course1img").show()
+    });
+
+    $("#course2img").click(function () {
+		$("#course2img").hide();
+		$("#iframe2").show();
+		});
+		$("#iframe2").click(function () {
+		$("#iframe2").hide();
+		$("#course2img").show();
+        });
+    
+    $("#course3img").click(function () {
+		$("#course3img").hide();
+		$("#iframe3").show();
+	});
+	$("#iframe3").click(function () {
+		$("#iframe3").hide();
+		$("#course3img").show();
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
