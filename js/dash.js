047822dd592bc7a36c4a916f67b0703b42fb0f3a
Geoffrey Gichaga <geoffrey.githinji@student.moringaschool.com>
2022-03-25 08:24:19 +0300

    ft:styling - add hover effects and videos on dashboard

--- js/dash.js
+++ js/dash.js
@@ -1,30 +1,17 @@
 $(document).ready(function () {
-    $("#course1img").click(function () {
-        $("#course1img").hide();
+    $("#img1").click(function () {
         $("#iframe1").show()
-    });
-    $("#iframe1").click(function () {
-        $("#iframe1").hide();
-        $("#course1img").show()
+		$("#l1").hide();
+
     });
 
-    $("#course2img").click(function () {
-		$("#course2img").hide();
-		$("#iframe2").show();
-		});
-		$("#iframe2").click(function () {
-		$("#iframe2").hide();
-		$("#course2img").show();
-        });
+    $("#img1").dbclick(function () {
+		$("#img1").show()
+
+        $("#iframe1").hide();
+    });
+
     
-    $("#course3img").click(function () {
-		$("#course3img").hide();
-		$("#iframe3").show();
-	});
-	$("#iframe3").click(function () {
-		$("#iframe3").hide();
-		$("#course3img").show();
-	});
 
 
 
